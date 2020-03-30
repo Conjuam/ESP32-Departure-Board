8fba7b77dd6f9d60847fdc8745f9d896d14c2556
Conjuam <Conjuam@users.noreply.github.com>
2020-03-30 17:36:22 +0100

    Remove redundant serial code

--- RTTAPI_ESP32/RTTAPI_ESP32.ino
+++ RTTAPI_ESP32/RTTAPI_ESP32.ino
@@ -5,17 +5,14 @@
 #include <ArduinoJson.h>
 // Uses ArduinoJson v.6.15.0
 
-//Also uses https://arduinojson.org/v6/assistant/
-//https://en.cppreference.com/w/cpp/chrono/c/strftime
-
-const char* ssid = "SSID";
-const char* pass = "PASSWORD";
+const char* ssid = "SSID"; //Substitute SSID with your WiFi SSID
+const char* pass = "PASSWORD"; //Substitute PASSWORD with your Wifi Password
 
 const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 0;
-const int   daylightOffset_sec = 3600;
+const long  gmtOffset_sec = 0; //Offset from GMT in seconds
+const int   daylightOffset_sec = 3600; //Daylight Saving Time Offset
 
-LiquidCrystal lcd(14, 27, 26, 25, 33, 32);
+LiquidCrystal lcd(14, 27, 26, 25, 33, 32); //LCD Pins
 
 void printLocalTime()
 {
@@ -25,7 +22,7 @@ void printLocalTime()
     lcd.print("Failed to obtain time");
     return;
   }
-  //Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  //Formatting options can be found here: https://en.cppreference.com/w/cpp/chrono/c/strftime
   lcd.print(&timeinfo, " %d/%m %H:%M:%S");
 }
 
@@ -38,13 +35,8 @@ void setup() {
 
   while (WiFi.status() != WL_CONNECTED) {
     //Serial.println("Establishing a connection with WiFi network");
-    lcd.print("Connecting...");
-    
+    lcd.print("Please Wait...");
   }
-  lcd.clear();
-  //Serial.println("Connection established");
-  //lcd.print("Connected!");
-  //delay(5000);
   lcd.clear();
 
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
@@ -55,45 +47,30 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  
   if ((WiFi.status() == WL_CONNECTED)) {
     HTTPClient http;
+    //Substitute username and password for your unique username and password
+    //Substitute "BMH" for your station's three-letter code
     http.begin("https://rttapi_username:rttapi_password@api.rtt.io/api/v1/json/search/BMH");
     int httpCode = http.GET();
 
-    //if (httpCode > 0) {
-      String payload = http.getString();
-      //Serial.println(httpCode);
-      //Serial.println(payload);
-      const size_t capacity = 18*JSON_ARRAY_SIZE(1) + JSON_ARRAY_SIZE(9) + 2*JSON_OBJECT_SIZE(3) + 18*JSON_OBJECT_SIZE(4) + 9*JSON_OBJECT_SIZE(9) + 9*JSON_OBJECT_SIZE(15);
-      DynamicJsonDocument doc(capacity);
-      deserializeJson(doc, payload);
-      //Serial.println();
-      //Serial.println();
-      //Serial.println();
-      //Serial.print("Next Service is for: ");
-      //Serial.print(doc["services"][0]["locationDetail"]["destination"][0]["description"].as<char*>());
-      //Serial.println();
-      //Serial.print("Due to arrive at: ");
-      //Serial.print(doc["services"][0]["locationDetail"]["realtimeArrival"].as<char*>());
-      //Serial.println();
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print(doc["services"][0]["locationDetail"]["realtimeArrival"].as<char*>());
-      lcd.setCursor(4,0);
-      lcd.print(": ");
-      lcd.setCursor(6,0);
-      lcd.print(doc["services"][0]["locationDetail"]["destination"][0]["description"].as<char*>());
-      lcd.setCursor(0,1);
-      printLocalTime();
-    //}
-    //else {
-      //Serial.println("HTTP Request Error");
-      //lcd.setCursor(0,0);
-      //lcd.print("HTTP ERROR");
-    //}
+    String payload = http.getString();
+    //The following line is a formula generated here: https://arduinojson.org/v6/assistant/
+    //Input a JSON response from a peak service time, to get one tailored to your station
+    const size_t capacity = 18*JSON_ARRAY_SIZE(1) + JSON_ARRAY_SIZE(9) + 2*JSON_OBJECT_SIZE(3) + 18*JSON_OBJECT_SIZE(4) + 9*JSON_OBJECT_SIZE(9) + 9*JSON_OBJECT_SIZE(15);
+    DynamicJsonDocument doc(capacity);
+    deserializeJson(doc, payload);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(doc["services"][0]["locationDetail"]["realtimeArrival"].as<char*>());
+    lcd.setCursor(4,0);
+    lcd.print(": ");
+    lcd.setCursor(6,0);
+    lcd.print(doc["services"][0]["locationDetail"]["destination"][0]["description"].as<char*>());
+    lcd.setCursor(0,1);
+    printLocalTime();
 
-    http.end();
+    http.end(); //Release resources
   }
   //delay(1000);
 }
